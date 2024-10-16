a848f7a6d02325290afbb53cee5be94925befe21
inv-Ayiba <tariogounga@gmail.com>
2024-10-16 19:28:22 +0100

    before chp 7 copy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { CitiesComponent } from './cities/cities.component';
 import { CountriesComponent } from './countries/countries.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CityEditComponent } from './cities/city-edit.component';
+import { CountryEditComponent } from './countries/country-edit.component';
 
 
 @NgModule({
@@ -22,7 +24,9 @@ import { CountriesComponent } from './countries/countries.component';
     HomeComponent,
     NavMenuComponent,
     CitiesComponent,
-    CountriesComponent
+    CountriesComponent,
+    CityEditComponent,
+    CountryEditComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
