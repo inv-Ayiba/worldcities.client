91dd1202f24fd1b7fa882fefef3706348b5f06d7
inv-Ayiba <tariogounga@gmail.com>
2024-11-01 17:19:54 +0100

    before Microsoft.AspNetCore.Identity.EntityFrameworkCore 8.0.1 jumped chp 9 and 10 btw

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { environment } from './../../environments/environment';
+// import { HttpClient, HttpParams } from '@angular/common/http';
+// import { environment } from './../../environments/environment';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -9,6 +9,7 @@ import { Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 import { City } from './city';
+import { CityService } from './city.service';
 
 @Component({
   selector: 'app-cities',
@@ -16,7 +17,7 @@ import { City } from './city';
   styleUrls: ['./cities.component.scss']
 })
 export class CitiesComponent implements OnInit {
-  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon', 'countryName'];
   public cities!: MatTableDataSource<City>;
 
   defaultPageIndex: number = 0;
@@ -32,7 +33,8 @@ export class CitiesComponent implements OnInit {
 
   filterTextChanged: Subject<string> = new Subject<string>();
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private cityService: CityService) {
   }
 
   ngOnInit() {
@@ -60,24 +62,30 @@ export class CitiesComponent implements OnInit {
   }
 
   getData(event: PageEvent) {
-    var url = environment.baseUrl + 'api/Cities';
-    var params = new HttpParams()
-      .set("pageIndex", event.pageIndex.toString())
-      .set("pageSize", event.pageSize.toString())
-      .set("sortColumn", (this.sort)
-        ? this.sort.active
-        : this.defaultSortColumn)
-      .set("sortOrder", (this.sort)
-        ? this.sort.direction
-        : this.defaultSortOrder);
 
-    if (this.filterQuery) {
-      params = params
-        .set("filterColumn", this.defaultFilterColumn)
-        .set("filterQuery", this.filterQuery);
-    }
+    var sortColumn = (this.sort)
+      ? this.sort.active
+      : this.defaultSortColumn;
 
-    this.http.get<any>(url, { params })
+    var sortOrder = (this.sort)
+      ? this.sort.direction
+      : this.defaultSortOrder;
+
+    var filterColumn = (this.filterQuery)
+      ? this.defaultFilterColumn
+      : null;
+
+    var filterQuery = (this.filterQuery)
+      ? this.filterQuery
+      : null;
+
+    this.cityService.getData(
+      event.pageIndex,
+      event.pageSize,
+      sortColumn,
+      sortOrder,
+      filterColumn,
+      filterQuery)
       .subscribe({
         next: (result) => {
           this.paginator.length = result.totalCount;
--- src/app/countries/country.ts
+++ src/app/countries/country.ts
@@ -1,6 +1,7 @@
 export interface Country {
-  id: number;
+  id: number;  
   name: string;
   iso2: string;
   iso3: string;
+  totCities: number;
 }
